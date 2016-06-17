0f3574df54fe9dc4e4d0a88a111f7787073af076
MessuKilkain <magniez.indiana@gmail.com>
2016-06-17 11:46:08 +0200

    Fix wrong comment in configuration

--- Pod/Classes/DZVideoPlayerViewControllerConfiguration.h
+++ Pod/Classes/DZVideoPlayerViewControllerConfiguration.h
@@ -10,7 +10,7 @@
 
 @interface DZVideoPlayerViewControllerConfiguration : NSObject
 
-@property (assign, nonatomic) BOOL isBackgroundPlaybackEnabled; // defaults to NO
+@property (assign, nonatomic) BOOL isBackgroundPlaybackEnabled; // defaults to YES
 @property (strong, nonatomic) NSMutableArray *viewsToHideOnIdle; // has topToolbarView and bottomToolbarView by default
 @property (assign, nonatomic) NSTimeInterval delayBeforeHidingViewsOnIdle; // defaults to 3 seconds
 @property (assign, nonatomic) BOOL isShowFullscreenExpandAndShrinkButtonsEnabled; // defaults to YES
